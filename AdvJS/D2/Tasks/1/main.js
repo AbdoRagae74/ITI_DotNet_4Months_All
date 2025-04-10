c01d767c86c71c1088793fe008df7371ba3dc840
AbdoRagae74 <abdelrahmanragae34@gmail.com>
2025-04-10 17:09:18 +0200

    Day2 Adv JS

---
+++ AdvJS/D2/Tasks/1/main.js
@@ -0,0 +1,42 @@
+function Rectangle(w, h) {
+    Rectangle.Count++;
+    this.width = Number(w);
+    this.height = Number(h);
+}
+Rectangle.Count=0;
+Rectangle.getCount = function(){
+    console.log(Rectangle.Count);
+}
+Rectangle.prototype.calculateArea = function () {
+     return this.width * this.height;
+ }
+Rectangle.prototype.calculatePerimeter = function () {
+  return 2*this.width +2*this.height;
+ }
+Rectangle.prototype.toString = function () {
+  return this.displayInfo();
+ }
+
+Rectangle.prototype.displayInfo = function () {
+
+    var area = this.calculateArea();
+    var perimeter = this.calculatePerimeter();
+
+    console.log(`width = ${this.width}\nheight = ${this.height}  `);
+    console.log(`area = ${area}\nperimeter = ${perimeter}  `);
+}
+
+// Rectangle.count = (isNaN(Rectangle.count)?0:Rectangle.count) +1;
+// Rectangle.count = (Rectangle.count || 0) + 1;
+
+
+var r1 = new Rectangle(12,7);
+var r2 = new Rectangle(12,7);
+var r3 = new Rectangle(12,7);
+var r4 = new Rectangle(12,7);
+var r5 = new Rectangle(12,7);
+console.log(Rectangle.getCount());
+
+
+
+
