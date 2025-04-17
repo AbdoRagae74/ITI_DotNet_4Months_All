554cf2a95416dc9445c8ff3577d3f346a8d20a37
AbdoRagae74 <abdelrahmanragae34@gmail.com>
2025-04-17 14:48:21 +0200

    ADV C#

---
+++ Adv C#/Day01/Day01-Task/Day01/Day01Session.cs
@@ -0,0 +1,107 @@
+﻿//namespace Day01
+//{
+//    //class Student
+//    //{
+//    //    public int id { get; set; }
+//    //    public string name { get; set; }
+//    //    public int age { get; set; }
+//    //    public Student(int id = 0, string name = "", int age = 0)
+//    //    {
+//    //        this.id = id;
+//    //        this.name = name;
+//    //        this.age = age;
+//    //    }
+//    //    public override string ToString()
+//    //    {
+//    //        return $"{id}-{name}-{age}years old";
+//    //    }
+//    //    public override bool Equals(object? obj)
+//    //    {
+//    //        Student s2 = (Student)obj;
+//    //        return (id==s2.id &&name==s2.name &&age==s2.age);
+//    //    }
+//    //    public override int GetHashCode()
+//    //    {
+//    //        return id;
+//    //    }
+
+
+//    //}
+//     struct complex
+//    {
+//        public int real { get; set; }
+//        public int img { get; set; }
+//            //public complex(int real, int img) 
+//            //{
+//            //    this .real = real;
+//            //    this .img = img;
+//            //}
+//            //public override string ToString()
+//            //{
+//            //    return $"{real}+{img}i";
+//            //}
+//    }
+//    //internal class Program
+//    //{
+//    //    static void Main(string[] args)
+//    //    {
+//    //        #region  system.object
+//    //        // employee em;
+//    //        // em.
+//    //        //// employee em = new employee();
+
+//    //        // test t;
+
+
+
+//    //        // object obj = new object();
+//    //        // obj = 3;
+//    //        // obj = "ali";
+//    //        // obj = 2.2f;
+
+//    //        //object[] arr = new object[4];
+//    //        //arr[0] = 1;
+//    //        //arr[1] = "ali";
+//    //        //arr[2] = 3.2f;
+//    //        //arr[3] = new employee();
+
+//    //        //int x = (int)arr[0];//unboxing
+//    //        //x++;
+//    //        //arr[0] = x;//boxing
+
+//    //        //tostring
+//    //        // Student s = new Student(1, "ali ahmed", 20);
+//    //        //// Console.WriteLine(s.ToString());
+
+
+//    //        // complex c1 = new complex(2, 3);
+//    //        // Console.WriteLine(c1.ToString());
+
+//    //        //equals
+
+//    //        complex c1 = new complex(1, 2);
+//    //        complex c2 = new complex(1, 2);
+
+//    //        Student s1 = new Student(1, "ali", 22);
+//    //        Student s2 = s1;
+//    //       // Student s2 = new Student(1, "ali", 22);
+
+//    //        //if (s1.Equals(s2))
+//    //        //{
+//    //        //    Console.WriteLine("equals");
+//    //        //}
+//    //        //else
+//    //        //{
+//    //        //    Console.WriteLine("not equals");
+//    //        //}
+
+//    //        //gettype
+//    //        //Console.WriteLine(s1.GetType().BaseType);
+
+//    //        Console.WriteLine(s1.GetHashCode());
+//    //        Console.WriteLine(s2.GetHashCode());
+
+//    //        #endregion
+//    //    }
+//    //}
+//}
---
+++ Adv C#/Day02/Day02Adv_C#/Day02Adv_C#/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02Adv_C_
+{
+    class Employee : IComparable
+    {
+
+        int id, salary;
+        string securityLevel, gender;
+        HiringDate comingDate;
+        public int Id { get { return id; } set { id = value; } }
+        public int Salary { get { return salary; } set { salary = value; } }
+        public string SLevel { get { return securityLevel; } set { securityLevel = value; } }
+        public string Gender { get { return gender; } set { gender = value; } }
+        public HiringDate ComingDate { get { return comingDate; } set { comingDate = value; } }
+        public Employee(int id = 0, int salary = 0, int m = 1, int d = 1, int y = 2000, string securityLevel = "", string gender = "")
+        {
+            this.id = id;
+            this.salary = salary;
+            this.securityLevel = securityLevel;
+            comingDate = new HiringDate(day: d, month: m, year: y);
+            this.gender = gender;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is Employee e)
+                return comingDate.CompareTo(e.comingDate);
+            throw new("Passed parameter isn't Employee");
+
+        }
+
+        public override string ToString()
+        {
+            return $"id : {id}\nSecurity Level : {securityLevel}\nGender : {gender}\nSalary : {salary}\ncoming date : {comingDate}";
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is Employee e)
+                return id == e.id;
+            return false;
+        }
+
+    }
+}
